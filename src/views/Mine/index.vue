2cd9a5cdc584eae9de3ca279e38afe2b66296482
ZachGuoMingyu <139160190@qq.com>
2020-04-23 13:08:57 +0800

    测试

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main">
+    <Header title="我的嘟嘟" />
+      <div id="content">
+        <Login />
+      </div>
+    <TabBar />
+  </div>
+</template>
+
+<script>
+
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+import Login from '@/components/Login'
+
+export default {
+  name:'Mine',
+  components:{
+    Header,
+    TabBar,
+    Login
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
